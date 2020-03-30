061d701bf7305c7fa1b7cdc4ffaaa00a32ae220e
v-crn <var.cyrano@gmail.com>
2020-03-30 23:14:58 +0900

    complete the section 1

--- 08_encrypt.py
+++ 08_encrypt.py
@@ -1,6 +1,6 @@
 def encrypt(s: str) -> str:
     """
-    与えられた文字列の各文字を，以下の仕様で変換する。
+    与えられた文字列の各文字を，以下の仕様で暗号化する。
     - 引数の文字（列）が英小文字ならば(219 - 文字コード)の文字に置換
     - その他の文字はそのまま出力
     """
@@ -12,9 +12,22 @@ def decrypt(s: str):
 
 
 def main():
+    print('Test 1')
     s = 'If I had words to make a day for you, I\'d sing you a morning golden and new.'
+    print('Original:', s)
     print('Encrypt:', encrypt(s))
+    print('Decrypt:', decrypt(s))
 
+    print('\nTest 2')
+    s = 'AaBbCc'
+    print('Original:', s)
+    print('Encrypt:', encrypt(s))
+    print('Decrypt:', decrypt(s))
+
+    print('\nTest 3')
+    s = 'Fall leaves after leaves fall'
+    print('Original:', s)
+    print('Encrypt:', encrypt(s))
     print('Decrypt:', decrypt(s))
 
 
---
+++ 09_random_sort_word.py
@@ -0,0 +1,32 @@
+import random
+
+
+def random_sort_word(word: str) -> str:
+    """
+    文字列に対して，各単語の先頭と末尾の文字は残し，それ以外の文字の順序をランダムに並び替える。ただし，長さが４以下の単語は並び替えない。
+    """
+    result = word
+    if len(word) > 4:
+        result = word[0] + ''.join(random.sample(word[1:-1], k=len(word) - 2)) + word[-1]
+    return result
+
+
+def random_sort(s: str) -> str:
+    """
+    スペースで区切られた単語列に対して，各単語の先頭と末尾の文字は残し，それ以外の文字の順序をランダムに並び替える。ただし，長さが４以下の単語は並び替えない。
+    """
+    return ' '.join(random_sort_word(s) for s in s.split())
+
+
+def main():
+    print('Test 1')
+    s = "wonderful"
+    print(random_sort_word(s))
+
+    print('\nTest 2')
+    s = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+    print(random_sort(s))
+
+
+if __name__ == '__main__':
+    main()
